537e1e8d194ef123acd32eee0d2d700b9cae5de7
igprad <ign.aldi.pradana@gmail.com>
2017-01-20 17:15:02 +0700

    added TodoRepositoryImpl.java and pom.xml

---
+++ demo-spring-boot/src/main/java/springboot/repository/TodoRepositoryImpl.java
@@ -0,0 +1,34 @@
+package springboot.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import springboot.model.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by indra.e.prasetya on 1/18/2017.
+ */
+@Service
+public class TodoRepository {
+
+  private static final Logger LOG = LoggerFactory.getLogger(TodoRepository.class);
+
+  private final List<Todo> todos = new ArrayList<Todo>();
+
+  public boolean store(Todo todo) {
+    LOG.debug("store...");
+    todos.add(todo);
+
+    return true;
+  }
+
+  public List<Todo> getAll() {
+    LOG.debug("getAll...");
+    List<Todo> result = new ArrayList<Todo>(todos);
+    LOG.debug("result:{}", result);
+    return result;
+  }
+}
