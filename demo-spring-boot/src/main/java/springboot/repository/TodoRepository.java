a07d3caa86d4ccacb32e5f32b20f99a4ee6cafa8
Putra Firman Ardiansyah <ardidroid97@gmail.com>
2017-01-20 11:33:47 +0700

    Adding saveTodo testing

--- demo-spring-boot/src/main/java/springboot/repository/TodoRepository.java
+++ demo-spring-boot/src/main/java/springboot/repository/TodoRepository.java
@@ -1,7 +1,9 @@
 package springboot.repository;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import springboot.model.Todo;
+import springboot.service.TodoService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +13,17 @@ import java.util.List;
  */
 @Service
 public class TodoRepository {
-
+	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(TodoRepository.class);
+	
   private final List<Todo> todos = new ArrayList<Todo>();
 
   public boolean store(Todo todo) {
+	LOG.debug("store....");
     todos.add(todo);
-
     return true;
   }
-
   public List<Todo> getAll() {
+	  LOG.debug("getAll...");
     return new ArrayList<Todo>(todos);
   }
 }
--- demo-spring-boot/src/main/java/springboot/service/TodoService.java
+++ demo-spring-boot/src/main/java/springboot/service/TodoService.java
@@ -1,5 +1,6 @@
 package springboot.service;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import springboot.model.Todo;
@@ -13,18 +14,25 @@ import java.util.List;
  */
 @Service
 public class TodoService {
-
+	
+	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(TodoService.class);
+	
   @Autowired
   private TodoRepository todoRepository;
 
   public boolean saveTodo(String name, TodoPriority priority) {
     Todo todo = new Todo(name, priority);
-
+    LOG.debug("saveTodo....");
+    LOG.debug("Data : " + todo.toString());
     return todoRepository.store(todo);
   }
 
-  public List<Todo> getAll() {
-    return todoRepository.getAll();
+  public List<Todo> getAll() {	  
+	  LOG.debug("getAll....");
+	  List<Todo> result = todoRepository.getAll();
+	  LOG.debug("result:{}", result);
+	  return result;
+	  //return todoRepository.getAll();
   }
 
 }
