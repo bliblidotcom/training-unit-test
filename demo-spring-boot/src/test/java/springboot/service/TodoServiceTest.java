7e55a5fb3f9cb49756d94bd7a7544d00f1e4fb76
AudineAmelly <adinatma@gmail.com>
2017-01-20 10:41:01 +0700

    latihan jUnit 2

--- demo-spring-boot/src/test/java/springboot/service/TodoServiceTest.java
+++ demo-spring-boot/src/test/java/springboot/service/TodoServiceTest.java
@@ -1,15 +1,23 @@
 package springboot.service;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import springboot.model.Todo;
+import springboot.model.constants.TodoPriority;
 import springboot.repository.TodoRepository;
 
 public class TodoServiceTest {
@@ -47,9 +55,15 @@ public class TodoServiceTest {
 		
 		//given, when, then merupakan behaviour
 		//given
+		List<Todo> todoList = new ArrayList<Todo>();
+		todoList.add(new Todo("one",TodoPriority.HIGH));
+		todoList.add(new Todo("two",TodoPriority.MEDIUM));
+		todoList.add(new Todo("three",TodoPriority.LOW));
+		BDDMockito.given(this.todoRepository.getAll()).willReturn(todoList);
 		//when
+		List<Todo> testResult = this.todoService.getAll();
 		//then
-		
+		Assert.assertThat(testResult, Matchers.equalTo(todoList));
 		this.todoService.getAll();
 		
 	}
