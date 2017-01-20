2f677819d8422a44b11bb49d3ee35d66006b53fd
salsa <azhalia.amesa@si.ukdw.ac.id>
2017-01-20 11:17:37 +0700

    To do service

--- demo-spring-boot/src/main/java/springboot/service/TodoService.java
+++ demo-spring-boot/src/main/java/springboot/service/TodoService.java
@@ -1,12 +1,14 @@
 package springboot.service;
+//nge save to do list sama ambil to do list apa aja
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import springboot.model.Todo;
 import springboot.model.constants.TodoPriority;
 import springboot.repository.TodoRepository;
-
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by indra.e.prasetya on 1/18/2017.
@@ -14,17 +16,22 @@ import java.util.List;
 @Service
 public class TodoService {
 
-  @Autowired
-  private TodoRepository todoRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(TodoService.class);
+    @Autowired
+    private TodoRepository todoRepository; //dependency
 
-  public boolean saveTodo(String name, TodoPriority priority) {
-    Todo todo = new Todo(name, priority);
+    public boolean saveTodo(String name, TodoPriority priority) {
+        Todo todo = new Todo(name, priority);
+        LOG.debug(".....Save to Do===========");
+        return todoRepository.store(todo);
+    }
 
-    return todoRepository.store(todo);
-  }
-
-  public List<Todo> getAll() {
-    return todoRepository.getAll();
-  }
+    public List<Todo> getAll() { //ambil semua todo 
+        LOG.debug("GetAll (service)");
+        List<Todo> result = todoRepository.getAll();
+      //  todoRepository.store(null);
+        LOG.debug("result:{}", result);
+        return result;
+    }
 
 }
---
+++ demo-spring-boot/src/test/java/springboot/service/TodoServiceTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springboot.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import org.hamcrest.Matchers;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import static org.mockito.BDDMockito.given;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.LoggerFactory;
+import springboot.model.Todo;
+import springboot.model.constants.TodoPriority;
+
+import springboot.repository.TodoRepository;
+
+/**
+ *
+ * @author Amesa
+ */
+public class TodoServiceTest {
+
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(TodoServiceTest.class);
+
+    @InjectMocks //add service, bikin obj baru
+    private TodoService todoService;
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @Before
+    public void setUp() { //sebelum method unit test
+        LOG.debug("==+before (Setup)============");
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @After
+    public void tearDown() { //setelah tiap method unit test
+        LOG.debug("===After....============");
+         Mockito.verifyNoMoreInteractions(this.todoRepository);
+    }
+
+    @Test
+    public void getAllTest() {
+        LOG.debug("===GET ALL TEST===========");
+
+        //given
+        List<Todo> todoList = new ArrayList<Todo>();
+        todoList.add(new Todo("one", TodoPriority.HIGH));
+        todoList.add(new Todo("two", TodoPriority.MEDIUM));
+        BDDMockito.given(this.todoRepository.getAll()).willReturn(todoList);
+
+        //when
+        List<Todo> testResult = this.todoService.getAll();
+
+        //then
+        //asert
+        Assert.assertThat(testResult == todoList, Matchers.equalTo(true));
+        Assert.assertThat(testResult, Matchers.equalTo(todoList));//mastiin objeknya persis sama
+        //verify
+        BDDMockito.then(this.todoRepository).should(BDDMockito.times(1)).getAll();
+        //Mockito.verifyNoMoreInteractions(this.todoRepository);
+    }
+
+    @Test
+    public void saveTodo() {
+          LOG.debug("====Save to Do===========");
+        //this.todoService.saveTodo(null,null);
+
+    }
+
+}
