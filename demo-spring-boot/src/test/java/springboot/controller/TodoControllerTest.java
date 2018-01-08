7a672ba10c2e174e47ea00ff8241a6efb5b3e374
Andi R Djunaedi <andi.r.djunaedi@gdn-commerce.com>
2018-01-08 12:29:15 +0700

    check response body content

--- demo-spring-boot/src/test/java/springboot/controller/TodoControllerTest.java
+++ demo-spring-boot/src/test/java/springboot/controller/TodoControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import springboot.model.Todo;
 import springboot.model.constants.TodoPriority;
@@ -16,14 +15,16 @@ import java.util.Arrays;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by indra.e.prasetya on 1/18/2017.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class TodoControllerTest {
 
   @MockBean
@@ -35,7 +36,8 @@ public class TodoControllerTest {
   private static final String NAME = "Todo1";
   private static final TodoPriority PRIORITY = TodoPriority.HIGH;
 
-  private static final String TODO = String.format("{\"name\":\"%s\",\"priority\":\"%s\"}", NAME, PRIORITY);
+  private static final String TODO = "{\"code\":200,\"message\":null,"
+      + "\"value\":[{\"name\":\"Todo1\",\"priority\":\"HIGH\"}]}";
 
   @Test
   public void all() {
@@ -49,6 +51,7 @@ public class TodoControllerTest {
       .then()
       .body(containsString("value"))
       .body(containsString(NAME))
+        .body(equalTo(TODO))
       .statusCode(200);
 
     verify(todoService).getAll();
